fea2dd3f37b0707e0a03dbe121348c254e4e8fc3
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-06-15 22:32:14 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,123 @@
-import logo from './logo.svg';
-import './App.css';
+import firebase from './firebaseConfig';
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [titulo, setTitulo] = useState('')
+  const [autor, setAutor] = useState('')
+  const [idPost, setIdPost] = useState('')
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    async function loadPosts() {
+      firebase.firestore().collection('posts').onSnapshot((snapshot => {
+        let lista = [];
+
+        snapshot.forEach(doc => {
+          lista.push({
+            id: doc.id,
+            titulo: doc.data().titulo,
+            autor: doc.data().autor
+          })
+        })
+
+        setPosts(lista)
+      }))
+    }
+
+    loadPosts()
+  }, [])
+
+  async function handleAdd() {
+    await firebase
+      .firestore().collection('posts')
+      .add({
+        titulo: titulo,
+        autor: autor
+      }).then(() => {
+        console.log('dados cadastrados :)')
+      })
+      .catch(err => {
+        console.log(err.message)
+      })
+      .finally(() => {
+        setAutor('')
+        setTitulo('')
+      })
+  }
+
+  async function buscarPost() {
+    await firebase.firestore().collection('posts')
+      .get()
+      .then(snapshot => {
+        let lista = [];
+
+        snapshot.forEach(doc => {
+          lista.push({
+            id: doc.id,
+            titulo: doc.data().titulo,
+            autor: doc.data().autor
+          })
+        })
+
+        setPosts(lista)
+      }).catch(() => console.log('deu algum erro'))
+  }
+
+  async function editarPost() {
+    await firebase.firestore().collection('posts')
+      .doc(idPost)
+      .update({
+        titulo: titulo,
+        autor: autor
+      }).then(() => {
+        alert('Dados Atualizados com sucessos')
+      })
+      .catch(err => {
+        console.log('Deu falha!!', + err)
+      }).finally(() => {
+        setAutor('')
+        setTitulo('')
+        setIdPost('')
+      })
+  }
+
+  async function excluirPost(id) {
+    await firebase.firestore().collection('posts')
+    .doc(id).delete()
+    .then(() => {
+      alert('post excluido')
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      E-mail:<input type="email"/> <br/>
+      Senha:<input type="text"/> <br/><br/>
+      <button>Logar</button> 
+      <button>Cadastrar</button> <br/><br/>
+
+      ID:<input type="text" value={idPost} onChange={e => setIdPost(e.target.value)} /> <br />
+      Titulo:<input value={titulo} type="text" onChange={(e) => setTitulo(e.target.value)} /> <br />
+      Autor:<input value={autor} type="text" onChange={(e) => setAutor(e.target.value)} /> <br />
+      <button onClick={buscarPost}>Buscar Post</button>
+      <button onClick={handleAdd}>Cadastrar</button>
+      <button onClick={editarPost}>Editar</button>
+      {
+        posts.map(post => {
+          return (
+            <ul key={post.id}>
+              <li>ID: {post.id}</li>
+              <li>Titulo: {post.titulo}</li>
+              <span>Autor: {post.autor}</span> <br />
+              <button onClick={() => excluirPost(post.id)}>Excluir</button>
+            </ul>
+          )
+        })
+      }
     </div>
   );
 }
+
+
 
 export default App;
---
+++ src/firebaseConfig.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDBAMwMWiGgKNalrBq7SgHDYyS-bu4XLDI",
+  authDomain: "curso-reactjs-f2250.firebaseapp.com",
+  projectId: "curso-reactjs-f2250",
+  storageBucket: "curso-reactjs-f2250.appspot.com",
+  messagingSenderId: "6018120120",
+  appId: "1:6018120120:web:bb2cd0e405e413ab07774d",
+  measurementId: "G-D30K0QPQRK"
+};
+
+if(!firebase.apps.length){
+  firebase.initializeApp(firebaseConfig)
+}
+
+export default firebase
